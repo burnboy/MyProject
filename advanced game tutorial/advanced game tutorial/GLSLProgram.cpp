5fd974e9cd651d3cad131d61ccdcb120024403f6
burnboy <uty1993@naver.com>
2017-08-16 21:25:03 +0900

    tutorial shader error
    create error session

--- advanced game tutorial/advanced game tutorial/GLSLProgram.cpp
+++ advanced game tutorial/advanced game tutorial/GLSLProgram.cpp
@@ -16,7 +16,19 @@ GLSLProgram::~GLSLProgram()
 void GLSLProgram:: compileShaders(const std::string &vertexShaderFilePath, const std::string &gragmentShaderFilepath)
 {
 	_vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-	if (_vertexShaderID)
+	if (_vertexShaderID==0)
+	{
+		fatalError("벌텍스셰이더<vertex shader> 만드는데에 실패했습니다!");
+	}
+
+
+	_fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	if (_fragmentShaderID==0)
+	{
+		fatalError("프레그먼트 셰이더<fragment shader> 만드는데에 실패했습니다!");
+	}
+
+
 
 }
 
